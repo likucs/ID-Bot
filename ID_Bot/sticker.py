dd65406b3f99fde5e5b14edd678efba2aae65aaf
Selfie SD <webtamil0@gmail.com>
2022-02-05 19:11:33 +0530

    Update sticker.py

--- ID_Bot/sticker.py
+++ ID_Bot/sticker.py
@@ -3,4 +3,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.private & filters.sticker)
 async def stickers(_, msg):
-    await msg.reply(f"This Sticker's ID is `{msg.sticker.file_id}`", quote=True)
+    await msg.reply(f"🃏 This Sticker's ID is `{msg.sticker.file_id}`", quote=True)
