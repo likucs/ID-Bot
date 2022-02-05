26ac6e09e3f28ce683fb4c45e6dcb511331f55e0
Selfie SD <webtamil0@gmail.com>
2022-02-05 19:16:41 +0530

    Update inline.py

--- ID_Bot/inline.py
+++ ID_Bot/inline.py
@@ -8,12 +8,12 @@ async def answer(_, inline_query):
 	await inline_query.answer(
 		results=[
 			InlineQueryResultArticle(
-				title=f"Your Telegram ID is {inline_query.from_user.id}",
+				title=f"✅ Your Telegram ID is {inline_query.from_user.id}",
 				input_message_content=InputTextMessageContent(
-					f"My Telegram ID is `{inline_query.from_user.id}`"
+					f"✅ My Telegram ID is `{inline_query.from_user.id}`"
 				),
-				description="Tap to send your ID to current chat",
-				thumb_url="https://telegra.ph/file/784c14c76533f944ae9b0.jpg",
+				description="Tap to send your ID to current chat 🙃",
+				thumb_url="https://telegra.ph/file/6939525d1291f0ec13c02.jpg",
 			)
 		],
 		cache_time=1,
